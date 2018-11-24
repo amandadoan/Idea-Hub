76f48f5447e1e07d418b1d34045a53bbb200a1f4
dletk <duc.mtle@gmail.com>
2018-11-24 04:37:56 -0600

    Added Post model

--- IdeaHub/userprofile/admin.py
+++ IdeaHub/userprofile/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
-from .models import MyUser, Project
+from .models import MyUser, Project, Post
 from .forms import CustomUserCreationForm
 
 class UserAdmin(admin.ModelAdmin):
-
-    # form = CustomUserCreationForm
-
     list_display = ("username", "first_name", "last_name", "email", "is_superuser", "is_staff")
     list_filter = ("is_superuser", "is_staff", "is_active")
 
+# TODO: CREATE model admin for Project and Post to manage easier
 
 # Register your models here.
 admin.site.register(MyUser, UserAdmin)
 admin.site.register(Project)
+admin.site.register(Post)
--- IdeaHub/userprofile/models.py
+++ IdeaHub/userprofile/models.py
@@ -45,8 +45,29 @@ class Project(models.Model):
     description = models.TextField(max_length=None, blank=False)
     created_date = models.DateTimeField(verbose_name="Created date", auto_now_add=True)
     end_date = models.DateTimeField(verbose_name="Ended date")
+    # The owner is the person who creatde the project, whereas the members are ones participating in the project
     owner = models.OneToOneField("MyUser", on_delete=models.CASCADE)
     members = models.ManyToManyField("MyUser", related_name="projects")
 
     def __str__(self):
         return self.project_name
+
+class Post(models.Model):
+    """
+    The model for a post. A post can be a comment, a question, or an update on the current project
+    """
+
+    TYPES = [("Q", "Question"), ("C", "Comment"), ("U", "Update")]
+
+    # Not allowed post without content
+    content = models.TextField(blank=False)
+    type = models.CharField(choices=TYPES, blank=False, default="C", max_length=2)
+    # Auto assigned the created time
+    time_posted = models.DateTimeField(auto_now_add=True)
+
+    # The user and project this post belongs to
+    user = models.ForeignKey("MyUser", on_delete=models.CASCADE)
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
