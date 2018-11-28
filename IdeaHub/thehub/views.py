16ceef627cac686ba4786dac07d5b6406710de02
nguyen-amanda <amanda@amandanguyen.com>
2018-11-27 20:11:36 -0600

    Add starting home page (WIP)

--- IdeaHub/thehub/views.py
+++ IdeaHub/thehub/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+from .models import Project, Post
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'thehub/home.html')
