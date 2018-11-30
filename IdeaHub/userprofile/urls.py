cc0c92ceebb654d5397b2426d769b38378f75352
julia.romare <julia.romare@gmail.com>
2018-11-29 20:14:13 -0600

    Fixed slashes for URLs

--- IdeaHub/userprofile/urls.py
+++ IdeaHub/userprofile/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.profile, name="profile"),
-    path("<str:username>", views.profile, name="profile"),
+    path("<str:username>/", views.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="userprofile/login.html"), name="login"),
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("logout/", auth_views.LogoutView.as_view(next_page='login'), name='logout'),
