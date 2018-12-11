a36aa2589befb179247ee90550deca025cb154b1
julia.romare <julia.romare@gmail.com>
2018-12-10 21:59:51 -0600

    Trying to fix respond

--- IdeaHub/thehub/urls.py
+++ IdeaHub/thehub/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("thehub/project/<str:project_name>/", views.project, name="project"),
-    path("thehub/post/<str:project_name>", views.makePost, name="makePost"),
+    path("thehub/post/<str:project_name>/", views.makePost, name="makePost"),
+    path("thehub/post/<str:project_name>/<int:parent_post_id>", views.makePost, name="makePost"),
+
     # The path without post_id is used to make sure template can be rendered without error
     path("thehub/getprojectupdate/<str:project_name>/<int:post_id>", views.getProjectUpdate, name="ajaxProjectUpdate"),
     path("thehub/getprojectupdate/<str:project_name>/", views.getProjectUpdate, name="ajaxProjectUpdate"),
