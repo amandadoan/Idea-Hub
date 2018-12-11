447b3c68c2744f25cd0189bd5a9ddbd70a9ddb2f
julia.romare <julia.romare@gmail.com>
2018-12-10 20:32:55 -0600

    Project's button frontend

--- IdeaHub/static/thehub/pop-ups.js
+++ IdeaHub/static/thehub/pop-ups.js
@@ -1,30 +1,20 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+$(".respond-button").click(function() {
+	$("#commentModal").show();
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("make-post");
-var cancelButton = document.getElementById("cancel");
+$("#make-post").click(function(event){
+	event.preventDefault();
+	$("#postModal").show();
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+$(".cancel").click(function(event) {
+	event.preventDefault();
+	$("#commentModal").hide();
+	$("#postModal").hide();
+});
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+$(".close").click(function(){
+	$("#commentModal").hide();
+	$("#postModal").hide();
+});
 
-cancelButton.onclick = function(e){
-	e.preventDefault();
-	modal.style.display = "none";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
