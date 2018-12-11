80326e7a409eee15c846445f8d8ba39aadc0b740
julia.romare <julia.romare@gmail.com>
2018-12-10 21:39:21 -0600

    Dynamically change modal form

--- IdeaHub/static/thehub/pop-ups.js
+++ IdeaHub/static/thehub/pop-ups.js
@@ -1,20 +1,20 @@
 $(".respond-button").click(function() {
-	$("#commentModal").show();
+	//$("#modalDisplay").html("")
+	//$("#modalDisplay").show();
 });
 
-$("#make-post").click(function(event){
-	event.preventDefault();
-	$("#postModal").show();
-});
+// $("#make-post").click(function(event){
+// 	event.preventDefault();
+// 	$("#modalDisplay").html("<div class='modal-content'><span class='close'>&times;</span></br><form action='" +"{% url 'makePost' project.project_name %}"+"' method='post'><div class='form-group'><label for='type'>Type:</label><select class='form-control' id='type' type='text' name='type'> <option value='C'>Comment</option><option value='Q'>Question</option> {% if canUpdate == True %} <option value='U'>Update</option> {% endif %} </select></div><div class='form-group'> <label for='content'>Message:</label><textarea name='content' id='content' class='form-control' rows='3'></textarea></div><input type='submit' class='btn btn-success float-right' value='Post'></input><button class='cancel btn btn-danger float-right'>Cancel</button> {% csrf_token %} </form></div>");
+// 	$("#modalDisplay").show();
+// });
 
 $(".cancel").click(function(event) {
 	event.preventDefault();
-	$("#commentModal").hide();
-	$("#postModal").hide();
+	$("#modalDisplay").hide();
 });
 
 $(".close").click(function(){
-	$("#commentModal").hide();
-	$("#postModal").hide();
+	$("#modalDisplay").hide();
 });
 
